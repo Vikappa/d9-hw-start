4e9209768238694929b5cb63b5563c8b65e348ea
Vincenzo Costantini <54511489+Vikappa@users.noreply.github.com>
2024-02-12 16:54:18 +0100

    fine

--- src/redux/reducers/mainReducerFavList.js
+++ src/redux/reducers/mainReducerFavList.js
@@ -5,11 +5,8 @@ const initialState = {
   };
   
   const mainReducer = (state = initialState, action) => {
-    // Handle different actions with switch-case
     switch (action.type) {
-      // Example case
-      // case 'ADD_TO_FAVLIST':
-      //   return { ...state, favList: { content: [...state.favList.content, action.payload] } };
+
       default:
         return state;
     }
